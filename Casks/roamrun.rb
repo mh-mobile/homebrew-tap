c867a1642ed66c32ff6e158afc00834530739126
mh-mobile <13811034+mh-mobile@users.noreply.github.com>
2026-09-26 09:55:02 +0900

    roamrun 0.1.8

--- Casks/roamrun.rb
+++ Casks/roamrun.rb
@@ -1,6 +1,6 @@
 cask "roamrun" do
-  version "0.1.7"
-  sha256 "2bf3d7434704c46f3e4986eb00ebcfd676e007effab3f193177f50ed6e39d6d9"
+  version "0.1.8"
+  sha256 "bc7eba7e89ad899d8a95c5e0431f2257d72c0a290bc8af93b00dd2306417cfc1"
 
   url "https://github.com/mh-mobile/RoamRun/releases/download/v#{version}/RoamRun-#{version}.dmg"
   name "RoamRun"
